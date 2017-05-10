e06beb12c8b8636b841369ffef84237c70cb7417
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-05-10 15:23:04 +0100

    done

--- httpcollect.js
+++ httpcollect.js
@@ -1,12 +1,35 @@
 var http = require('http');
-var bl = require('bl');
-
 var url = process.argv[2];
+
+var info = '';
 
 
 http.get(url, function (response) {
-	response.setEncoding('utf8').on("data", function (data){
-		console.log(data);
-		res.end();
-	})
+	response.setEncoding('utf8')
+	response.on('data', function (data){
+		info += data.toString();
+	});
+	response.on('end', function (){		
+		console.log(info.length);
+		console.log(info);
+		info = '';
+	});
+	response.on('error', console.error)
+}).on('error', console.error)
+
+//Oficial Solution
+/*
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+	response.pipe(bl(function (err, data) {
+		if (err) {
+			return console.error(err)
+		}
+		data = data.toString()
+		console.log(data.length)
+		console.log(data)
+	}))
 });
+*/
