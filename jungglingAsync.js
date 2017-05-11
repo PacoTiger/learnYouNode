d671088f3e662378f990d5dca8b14d8ed7024b3d
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-05-11 16:08:58 +0100

    fixed

--- jungglingAsync.js
+++ jungglingAsync.js
@@ -8,8 +8,10 @@ var arrayB = [];
 var arrayC = [];
 
 http.get(urlA, function (res) {
+	//console.log('peticion url A');
 	res.setEncoding('utf8')
 	res.on('data', function (data) {
+		//console.log("recibiendo A <> ")
 		arrayA.push(data);
 	});
 	res.on('end', function () {
@@ -18,8 +20,10 @@ http.get(urlA, function (res) {
 }).on('error', console.error);
 
 http.get(urlB, function (res) {
+	//console.log('peticion url B');
 	res.setEncoding('utf8')
 	res.on('data', function (data) {
+		//console.log("recibiendo B <> ")
 		arrayB.push(data);
 	});
 	res.on('end', function () {
@@ -28,6 +32,7 @@ http.get(urlB, function (res) {
 }).on('error', console.error);
 
 http.get(urlC, function (res) {
+	
 	res.setEncoding('utf8')
 	res.on('data', function (data) {
 		arrayC.push(data);
@@ -36,3 +41,38 @@ http.get(urlC, function (res) {
 		console.log(arrayC.join("").toString());
 	});
 }).on('error', console.error);
+
+
+//OFICIAL SOLUTION
+
+/*var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResult () {
+	for (var i = 0; i > 3; i++) {
+		console.log(results[i])
+	}
+}
+
+function httpGet (index) {
+	http.get(process.argv[2 + index], function (response) {
+		response.pipe(bl(function (err, data) {
+			if (err) {
+				return console.error(err)
+			}
+
+			results[index] = data.toString()
+			count++
+
+			if (count === 3) {
+				printResults()
+			}
+		}))
+	})
+}
+
+for (var i = 0; i < 3; i++) {
+	httpGet(i)
+}*/
