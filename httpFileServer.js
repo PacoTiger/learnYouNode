3882c8bfd3ed4519329e1155d8629a6d626dd6c7
Francisco Padilla Muñoz <padilla.fco@gmail.com>
2017-05-22 15:00:58 +0100

    Solved

--- httpFileServer.js
+++ httpFileServer.js
@@ -4,11 +4,15 @@ var fs = require('fs');
 var port = process.argv[2];
 var file = process.argv[3];
 
-http.createServer( function (req, res) {
-	//console.log('client connected at port ' + port);
-	console.log('Server running at http://127.0.0.1:1337/');
-	console.log('file found in ' + file);
 
-	console.info('res > ' + res);
-	console.info('req > ' + req);
-}).listen(1337, '127.0.0.1')
+http.createServer( function (req, res) {
+	
+	console.log('Server running at ' + port);
+
+	//it's optional
+	res.writeHead(200, {'content-type': 'text/plain'})
+
+	fs.createReadStream(file).pipe(res)
+
+
+}).listen(port)
